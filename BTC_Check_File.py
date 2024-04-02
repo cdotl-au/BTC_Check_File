37ba703239a27371c6a116ee7106437fefd81e26
cdotl <31232628+citylogic-au@users.noreply.github.com>
2024-04-02 21:02:31 +1100

    Add files via upload

---
+++ BTC_Check_File.py
@@ -0,0 +1,142 @@
+import winsound
+import time
+
+from bitcoin import sha256, privtopub, pubtoaddr
+from bitcoinaddress import Wallet
+
+iAttempts = 0
+bFound = False
+
+import datetime
+
+def write_log(log_message, file_name='log.txt'):
+
+    #Get current date and time
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime('%d-%m-%Y %H:%M:%S')
+
+    #Construct the log message
+    full_message = f"{formatted_time} - {log_message}\n"
+
+    #Write to the log file
+    with open(file_name, 'a') as file:
+        file.write(full_message)
+
+    print(f"Log entry added: {full_message}")
+
+def IsFound(mnemonic):
+
+    global iAttempts
+
+    #print(f"Mnemonic: {mnemonic}")
+
+    private_key = sha256(mnemonic)
+
+    wallet = Wallet(private_key)
+    #print(wallet)
+
+    btc_address = wallet.address.mainnet.pubaddr1
+    btc_addressc = wallet.address.mainnet.pubaddr1c
+    btc_address3 = wallet.address.mainnet.pubaddr3
+    btc_address4 = wallet.address.mainnet.pubaddrbc1_P2WPKH
+    btc_address5 = wallet.address.mainnet.pubaddrbc1_P2WSH
+
+    if iAttempts % 100 == 0:
+        #print(iAttempts, btc_address, btc_addressc, btc_address3, btc_address4, btc_address5, mnemonic)
+        
+        tmp = f"{iAttempts:,} {mnemonic}"
+        print(tmp)
+        write_log(tmp)
+
+    if btc_address in publist or btc_addressc in publist or btc_address3 in publist or btc_address4 in publist or btc_address5 in publist:
+
+        log_message = f"Mnemonic: {mnemonic}\nAddress: {btc_address}\nAddress Compressed: {btc_addressc}\n Address 3: {btc_address3}\n Address 4: {btc_address4}\n Address 5: {btc_address5}\nPrivate Key WIF: {wallet.key.mainnet.wif}\nPrivate Key WIF Compressed: {wallet.key.mainnet.wifc}"
+
+        with open('win.txt', 'a') as file:
+            file.write(log_message + '\n')
+
+        print(f"Found {btc_address} {btc_addressc}")
+
+        return True
+    else:
+
+        #print(f"Not Found {btc_address} {btc_addressc}")
+        return False
+
+def Test(mnemonic):
+    private_key = sha256(mnemonic)
+    wallet = Wallet(private_key)
+    print(f"Bitcoin Address: {wallet.address.mainnet.pubaddr1}")
+    print(f"Bitcoin Address Compressed: {wallet.address.mainnet.pubaddr1c}")
+    print(f"Private Key WIF: {wallet.key.mainnet.wif}")
+    print(f"Private Key WIF Compressed: {wallet.key.mainnet.wifc}")
+    return
+
+def myFound():
+    global bFound
+    bFound = True
+    for i in range(10):
+        winsound.PlaySound('success.wav', winsound.SND_FILENAME)
+        time.sleep(3)  # Sleeps for 3 seconds
+
+##########################################################################################
+
+#addresses_filename = "blockchair_bitcoin_addresses_and_balance_LATEST.tsv"
+addresses_filename = "wallets.txt"
+
+winsound.PlaySound('success.wav', winsound.SND_FILENAME)
+
+print("BTC")
+print(f'Loading "{addresses_filename}"...')
+
+with open(addresses_filename) as f:
+    #publist = frozenset(line.strip().split('\t')[0] for line in f) #use this if you want to load the whole tsv and not a subset
+    publist = frozenset(line.strip() for line in f)
+
+print("Loaded.")
+iTotalLines = len(publist)
+print(f'Length: {iTotalLines}')
+
+iLines = 0
+
+replace_chars = ['_', '!', '*', '$']  #Characters to replace spaces with
+
+with open('passphrases.txt', 'r') as file:
+    for line in file:
+        original_line = line.strip()
+
+        variants = [original_line]
+
+        # Add lower, upper, and title case variants
+        for case_function in [str.lower, str.upper, str.title]:
+            case_variant = case_function(original_line)
+            if case_variant != original_line:
+                variants.append(case_variant)
+
+        # Variants without spaces and with space replaced by each character in replace_chars
+        for replace_char in [''] + replace_chars:  # Include empty string to remove spaces
+            replaced_line = original_line.replace(' ', replace_char)
+            if replaced_line != original_line:
+                variants.append(replaced_line)
+
+                # Add lower, upper, and title case variants of the replaced line
+                for case_function in [str.lower, str.upper, str.title]:
+                    case_variant = case_function(replaced_line)
+                    if case_variant not in variants:  # Avoid duplicates
+                        variants.append(case_variant)
+
+        # Check each variant
+        for variant in variants:
+            iAttempts += 1
+            if IsFound(variant):
+                myFound()
+                break
+
+        iLines += 1  # Only increment if no match was found
+
+if bFound:
+    print("Winner Winner Chicken Dinner!")
+else:
+    print("Better luck next time!")
+
+print(f"Attempts: {iAttempts} for {iLines} lines")
---
+++ TSV_Split.py
@@ -0,0 +1,67 @@
+import csv
+
+def separate_column(input_file, column_index, output_file, row_limit=None, ignore_first_row=False, stop_value=None):
+    with open(input_file, 'r', newline='') as f_input, open(output_file, 'w', newline='') as f_output:
+        reader = csv.reader(f_input, delimiter='\t')
+        
+        # Skip the first row if ignore_first_row is True
+        if ignore_first_row:
+            next(reader)
+            
+        rows_processed = 0
+        for row in reader:
+            # Stop processing after row_limit rows
+            if row_limit and rows_processed >= row_limit:
+                break
+            
+            column_value = row[column_index].rstrip('\r\n')
+
+            if stop_value is not None and float(row[1]) < stop_value:
+                print(f"Stopping program because column 1 exceeds {stop_value}")
+                return
+                
+            if rows_processed == 0:
+                f_output.write(column_value)
+            else:
+                f_output.write(' ' + column_value)
+                
+            rows_processed += 1
+            print(rows_processed, row[1])
+            
+def separate_rows(input_file, column_index, output_file, row_limit=None, ignore_first_row=False, stop_value=None):
+    with open(input_file, 'r', newline='') as f_input, open(output_file, 'w', newline='') as f_output:
+        reader = csv.reader(f_input, delimiter='\t')
+        
+        # Skip the first row if ignore_first_row is True
+        if ignore_first_row:
+            next(reader)
+            
+        rows_processed = 0
+        for row in reader:
+            # Stop processing after row_limit rows
+            if row_limit and rows_processed >= row_limit:
+                break
+            
+            column_value = row[column_index].rstrip('\r\n')
+
+            if stop_value is not None and float(row[1]) < stop_value:
+                print(f"Stopping program because column 1 exceeds {stop_value}")
+                return
+                
+            f_output.write(column_value + "\n")
+                
+            rows_processed += 1
+            print(rows_processed, row[1])
+            
+            
+input_file = "blockchair_bitcoin_addresses_and_balance_LATEST.tsv"
+
+output_file = "wallets.txt"
+column_index = 0
+row_limit = 50000000
+ignore_first_row = True
+stop_value = 2000000
+
+separate_rows(input_file, column_index, output_file, row_limit, ignore_first_row, stop_value)
+#separate_rows(input_file, column_index, output_file, row_limit, ignore_first_row)
+
